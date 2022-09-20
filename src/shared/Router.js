77a47edb2d90ff0ecab09d96d957ab4545468116
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-20 15:03:31 +0900

    [Feat] Places.jsx 생성.

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "../pages/mainpage/MainPage";
 import NotFound from "../pages/NotFound";
+import Places from "../pages/mainpage/Places";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+          <Route path="/place" element={<Places/>}/>
           <Route path="*" element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
