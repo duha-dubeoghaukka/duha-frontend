1df24d039f3dd00ae01ae0468faa041bf32a8559
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-20 15:12:40 +0900

    [Feat] placeNames.json 생성.

--- src/pages/mainpage/Places.jsx
+++ src/pages/mainpage/Places.jsx
@@ -1,5 +1,19 @@
 const Places = () => {
-    return <div></div>
+    return <div>
+        <div>
+            <ul className="flex flex-row justify-around">
+                <li className="font-bold text-2xl">관광</li>
+                <li className="font-bold text-2xl">맛집</li>
+                <li className="font-bold text-2xl">숙소</li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li>전체</li>
+                
+            </ul>
+        </div>
+    </div>
 }
 
 export default Places;
