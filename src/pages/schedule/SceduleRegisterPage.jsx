ba8f55b59a37f0bc5205fcce37c7d5f7b312ef13
marksenee <mark233891@gmail.com>
2022-09-21 10:15:22 +0900

    Feat: 일정 등록 (작업 진행중)

--- src/pages/schedule/SceduleRegisterPage.jsx
+++ src/pages/schedule/SceduleRegisterPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+// import 'react-calendar/dist/Calendar.css';
+import './styles.css';
+import moment from 'moment';
 
 function ScheduleRegisterPage() {
   const [value, onChange] = useState(new Date());
@@ -8,8 +10,12 @@ function ScheduleRegisterPage() {
   return (
     <>
       <div>
-        <Calendar onChange={onChange} value={value} />
-        {/* <div className="text-gray-500 mt-4">{moment(value).format('YYYY년 MM월 DD일')}</div> */}
+        <Calendar onChange={onChange} value={value} selectRange={true} formatDay={(locale, date) => moment(date).format('DD')} minDetail="month" maxDetail="month" />
+        <div className="text-gray-500 mt-4">{moment(value).format('YYYY년 MM월 DD일')}</div>
+        <div className="flex flex-col">
+          <input placeholder="출발하는 날짜를 입력해주세요" />
+          <input placeholder="돌아오는 날짜를 입력해주세요" />
+        </div>
       </div>
     </>
   );
