885d2bfd896b51f81e4b4b31a5e15be896bbed4f
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-21 17:52:15 +0900

    [Feat] 지역 구분 변경: 제주 시내, 애월, 중문, 서귀포, 우도&성산, 구좌&조천

--- src/utils/regionNames.js
+++ src/utils/regionNames.js
@@ -4,16 +4,28 @@ const regionNames = [
     romanization: "All"
   },
   {
-    name: "제주",
-    romanization: "Jeju"
+    name: "제주 시내",
+    romanization: "Downtown Jeju"
+  },
+  {
+    name: "애월",
+    romanization: "Aewol"
+  },
+  {
+    name: "중문",
+    romanization: "Jungmun"
   },
   {
     name: "서귀포",
     romanization: "Seogwipo"
   },
   {
-    name: "우도",
-    romanization: "Udo"
+    name: "우도&성산",
+    romanization: "Udo & Seongsan"
+  },
+  {
+    name: "구좌&조천",
+    romanization: "Gujwa & Jocheon"
   }
 ];
 
