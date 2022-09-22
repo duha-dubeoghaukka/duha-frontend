093193918f25aece3549a494336387cfab9b3e33
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-23 08:48:13 +0900

    [Fix] 모바일 버전 패딩 수정.

--- src/components/mainpage/Item.jsx
+++ src/components/mainpage/Item.jsx
@@ -3,8 +3,8 @@ import FavoriteRoundedIcon from "@mui/icons-material/FavoriteRounded";
 const Item = ({ data }) => {
   const { name, description, region, likeNum, thumbnailUrl } = data;
   return (
-    <div className="bg-white1 p-[20px] rounded-xl mb-[32px] shadow-md flex justify-between items-center">
-      <div className="pl-[50px]">
+    <div className="p-[15px] bg-white1 md:p-[20px] rounded-xl mb-[32px] shadow-md flex justify-between items-center">
+      <div className="pl-[5px] md:pl-[50px]">
         <div className="mb-[4px]">
           <p className="font-bold text-[20px]">{name}</p>
         </div>
@@ -19,7 +19,7 @@ const Item = ({ data }) => {
           <p className="text-[12px]">{likeNum}</p>
         </div>
       </div>
-      <div className="ml-3 w-[220px] h-[110px] flex-shrink-0">
+      <div className="ml-3 w-[150px] h-[90px] md:w-[220px] md:h-[110px] flex-shrink-0">
         <img loading="lazy" className="w-full h-full object-cover object-center rounded-xl" src={thumbnailUrl} alt={name} />
       </div>
     </div>
