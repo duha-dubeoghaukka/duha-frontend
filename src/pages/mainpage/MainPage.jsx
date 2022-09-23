ebc03940a5c0adfee2008f4919c825211b48ccef
marksenee <mark233891@gmail.com>
2022-09-23 19:06:39 +0900

    Feat: 메인페이지(홈) 추가 및 ui 적용

--- src/pages/mainpage/MainPage.jsx
+++ src/pages/mainpage/MainPage.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
+import Layout from "../../components/layout/Layout";
+import HomeCard from "../../components/mainpage/HomeCard";
 
 function MainPage() {
   return (
-    <>
-      <div>메인페이지</div>
-    </>
+    <Layout isLoggedIn={false} title="메인 페이지" highlight={"mainpage/home"}>
+      <div className="grid place-items-center h-screen">
+        <span className="font-bold">오늘 제주 날씨는 </span>
+        <HomeCard />
+      </div>
+    </Layout>
   );
 }
 
