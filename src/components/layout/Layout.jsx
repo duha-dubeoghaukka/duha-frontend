d45d0ddaaa422085fa1015350939ffd558c546da
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-09-24 16:00:08 +0900

    Fix: router 변경

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -80,7 +80,7 @@ const Layout = props => {
                 <h2 className="font-bold text-[20px] ml-[6px]">일정</h2>
               </div>
               <div className="text-base ml-[38px] flex flex-col">
-                <Link to="/schedule" className={`cursor-pointer ${category === "create" && "text-green1"}`}>
+                <Link to="/schedule/register" className={`cursor-pointer ${category === "create" && "text-green1"}`}>
                   일정 등록
                 </Link>
                 <Link to="/" className={`cursor-pointer ${category === "share" && "text-green1"}`}>
