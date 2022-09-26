71845a1c7e19b8f30f4067a245a5b72eab0523a6
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-26 10:40:03 +0900

    [Feat] 디테일 페이지 헤더 추가.

--- src/pages/detailpage/TouristSpotDetailPage.jsx
+++ src/pages/detailpage/TouristSpotDetailPage.jsx
@@ -1,7 +1,25 @@
-const TouristSpotDetailPage = () => {
-  return <div>
+import { useParams } from "react-router-dom";
+import Layout from "../../components/layout/Layout";
+import FavoriteRoundedIcon from "@mui/icons-material/FavoriteRounded";
+import StarOutlineRoundedIcon from "@mui/icons-material/StarOutlineRounded";
+// import StarRoundedIcon from "@mui/icons-material/StarRounded";
 
-  </div>
-}
+const TouristSpotDetailPage = () => {
+  const { spotID } = useParams();
+  return (
+    <Layout isLoggedIn={false} title="관광지 상세" highlight="mainpage/spots">
+      <div className="flex justify-between">
+        <div className="flex items-center">
+          <h2 className="font-bold text-[26px] mr-1">관광지명</h2>
+          <p className="text-[20px] mr-1">(999)</p>
+          <FavoriteRoundedIcon sx={{ color: "red" }} />
+        </div>
+        <div>
+          <StarOutlineRoundedIcon fontSize="large" />
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default TouristSpotDetailPage;
