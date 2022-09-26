255351578e847a393578ff56226dd4afc4ad3824
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-26 10:56:38 +0900

    [Feat] 리뷰 아이템 추가.

---
+++ src/pages/detailpage/ReviewItem.jsx
@@ -0,0 +1,20 @@
+const ReviewItem = () => {
+  return (
+    <div>
+      <div className="flex items-center mb-[28px]">
+        <div className="mr-[16px]">
+          <p>사진</p>
+        </div>
+        <div>
+          <p>닉네임</p>
+          <p>리뷰 등록일</p>
+        </div>
+      </div>
+      <div>
+        <p>설명란</p>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewItem;
--- src/pages/detailpage/TouristSpotDetailPage.jsx
+++ src/pages/detailpage/TouristSpotDetailPage.jsx
@@ -3,13 +3,14 @@ import Layout from "../../components/layout/Layout";
 import FavoriteRoundedIcon from "@mui/icons-material/FavoriteRounded";
 import StarOutlineRoundedIcon from "@mui/icons-material/StarOutlineRounded";
 import MapIcon from "@mui/icons-material/Map";
+import ReviewItem from "./ReviewItem";
 // import StarRoundedIcon from "@mui/icons-material/StarRounded";
 
 const TouristSpotDetailPage = () => {
   const { spotID } = useParams();
   return (
     <Layout isLoggedIn={false} title="관광지 상세" highlight="mainpage/spots">
-      <div className="flex justify-between">
+      <div className="flex justify-between mb-[20px]">
         <div className="flex items-center">
           <h2 className="font-bold text-[26px] mr-1">관광지명</h2>
           <p className="text-[20px] mr-1">(999)</p>
@@ -19,7 +20,7 @@ const TouristSpotDetailPage = () => {
           <StarOutlineRoundedIcon fontSize="large" />
         </div>
       </div>
-      <div>
+      <div className="mb-[20px]">
         <div className="flex justify-between align-top px-[25px] py-[36px] border-green1 border-[1px] rounded-lg">
           <div>
             <p className="mb-[26px]">주소</p>
@@ -37,21 +38,14 @@ const TouristSpotDetailPage = () => {
         </div>
       </div>
       <div>
-        <div>
-          <h3>리뷰</h3>
-        </div>
         <div className="p-[32px] bg-white1 rounded-lg">
-          <div className="flex items-center mb-[28px]">
-            <div className="mr-[16px]">
-              <p>사진</p>
-            </div>
-            <div>
-              <p>닉네임</p>
-              <p>리뷰 등록일</p>
-            </div>
+          <div className="mb-[24px]">
+            <h3>리뷰</h3>
           </div>
-          <div>
-            <p>설명란</p>
+          <div className="grid gap-[44px]">
+            <ReviewItem />
+            <ReviewItem />
+            <ReviewItem />
           </div>
         </div>
       </div>
