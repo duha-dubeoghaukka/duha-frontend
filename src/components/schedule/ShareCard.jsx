69f5644cf2ec34e438e5af84023676913f35b6e3
marksenee <mark233891@gmail.com>
2022-09-27 14:47:24 +0900

    Feat: ui 수정

--- src/components/schedule/ShareCard.jsx
+++ src/components/schedule/ShareCard.jsx
@@ -13,7 +13,7 @@ function ShareCard() {
 
   return (
     <div className="grid place-items-center h-screen">
-      <span className="m-3 font-bold">다른 뚜벅이들의 일정을 참고해보세요!</span>
+      <span className="m-5 font-normal text-lg">다른 뚜벅이들의 제주 일정을 참고해보세요!</span>
       {shareData?.map(item => {
         return <ShareCardComponent key={item.id} item={item} />;
       })}
@@ -29,13 +29,13 @@ function ShareCardComponent({ item }) {
     <div className="group w-96 h-28 bg-white1 rounded-md shadow-lg mt-5 flex flex-row hover:bg-green1">
       <div className="flex space-x-20">
         <div className="flex flex-col m-6 p-3 w-48">
-          <span className="mt-2	font-bold group-hover:text-white1">{title}</span>
+          <span className="font-bold group-hover:text-white1">{title}</span>
           <span className="mt-2	font-base text-xs group-hover:text-white1">
             {startAt}-{endAt}
           </span>
         </div>
         <div className="flex flex-row m-5 ">
-          <NavigateNextIcon className="mt-5 cursor-pointer group-hover:fill-white1" onClick={() => navigate(`${route}`)} />
+          <NavigateNextIcon className="mt-5 cursor-pointer group-hover:fill-white1" />
         </div>
       </div>
     </div>
--- src/pages/schedule/ShareSchedulePage.jsx
+++ src/pages/schedule/ShareSchedulePage.jsx
@@ -5,7 +5,7 @@ import ShareCard from "../../components/schedule/ShareCard";
 function ShareSchedulePage() {
   return (
     <Layout isLoggedIn={false} title="일정 공유" highlight={"schedule/share"}>
-      <ShareCard>일정 공유 페이지 입니다</ShareCard>
+      <ShareCard />
     </Layout>
   );
 }
