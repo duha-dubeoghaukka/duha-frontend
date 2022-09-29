c2169b4cc083f2c090b2809f11e786289a664d8f
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-29 13:10:18 +0900

    [Remove] useCounter.js 잔재 제거.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Router from "./shared/Router";
 import GlobalState from "./shared/GlobalState";
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import useCounter from "./hooks/useCounter";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +10,6 @@ function App() {
   const [currentSpotPage, setCurrentSpotPage] = useState(1);
   const [currentRestaurantPage, setCurrentRestaurantPage] = useState(1);
   const [isMapModalOpen, setIsMapModalOpen] = useState(false);
-  const [counter, increaseCounter] = useCounter();
   const globalStates = {
     regionSelection: {
       selectedRegion,
