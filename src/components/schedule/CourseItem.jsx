528fe254751d811be1b189d8cd462ceee1a64070
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-09-30 14:20:30 +0900

    Fix: day1 day2 클릭시 저장된 코스 불러오기

--- src/components/schedule/CourseItem.jsx
+++ src/components/schedule/CourseItem.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
-const CourseItem = () => {
+const CourseItem = ({ dayCourse, currentDay, currentCourseId }) => {
+  if (dayCourse.length === 0) return <div className="mb-6 text-center font-bold text-base">{currentDay}일차에 등록된 코스가 없습니다.</div>;
   return (
-    <div className="flex items-center mb-9">
-      <div className="bg-green1 rounded-full w-12 h-12 text-center font-bold text-lg text-white1 pt-[10px]">1</div>
-      <div className="my-2 ml-9">
-        <div className="font-bold text-base">관광</div>
-        <div className="font-bold text-base">관광지 이름</div>
-      </div>
+    <div>
+      {dayCourse.map(course => (
+        <div className="flex items-center mb-9">
+          <div className="bg-green1 rounded-full w-12 h-12 text-center font-bold text-lg text-white1 pt-[10px]">{course.detailOrder}</div>
+          <div className="my-2 ml-9">
+            <div className="font-semibold text-sm">{course.category}</div>
+            <div className="font-bold text-base">{course.name}</div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/schedule/DayItem.jsx
+++ src/components/schedule/DayItem.jsx
@@ -1,21 +1,28 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { api } from "../../api/api";
 import CourseItem from "./CourseItem";
+import _, { find } from "lodash";
 
 const DayItem = () => {
+  const [courses, setCourses] = useState([]);
+  const [dayCourse, setDayCourse] = useState([]);
+  const [currentDay, setCurrentDay] = useState(1);
+  const [currentCourseId, setCurrentCourseId] = useState();
   const navigate = useNavigate();
-
-  const day = localStorage.getItem("day");
   const id = localStorage.getItem("id");
 
-  const dayArr = Array(parseInt(day))
-    .fill()
-    .map((_, i) => i + 1);
+  const onClickDay = day => {
+    const clickDay = _.find(courses, { day: day });
+    setDayCourse(clickDay.courseDetails);
+    setCurrentDay(clickDay.day);
+    setCurrentCourseId(clickDay.courseId);
+  };
 
   const fetchData = async () => {
     try {
-      await api.get(`/auth/trip/${id}`);
+      const { data } = await api.get(`/auth/trip/${id}`);
+      setCourses(data.data.courses);
     } catch (error) {
       throw new Error(error);
     }
@@ -28,22 +35,26 @@ const DayItem = () => {
   return (
     <div>
       <div className="mt-4 mb-6 flex justify-center">
-        {dayArr.map(day => {
+        {courses.map(course => {
           return (
-            <div key={day} className="text-green1 font-bold text-lg px-4">
-              Day{day}
+            <div
+              key={course.courseId}
+              className="text-green1 font-bold text-lg px-4 cursor-pointer"
+              onClick={() => {
+                onClickDay(course.day);
+              }}
+            >
+              Day{course.day}
             </div>
           );
         })}
       </div>
       <div className="course-layout">
-        <CourseItem />
-        <CourseItem />
-        <CourseItem />
-        <button className="btn-primary py-3" onClick={() => navigate("addspot")}>
-          장소 추가
+        <CourseItem dayCourse={dayCourse} currentDay={currentDay} currentCourseId={currentCourseId} />
+        <button className="btn-primary py-3" onClick={() => navigate(`${currentCourseId}/addspot`)}>
+          코스 추가
         </button>
-        <button className="btn-primary py-3 mt-4">저장 하기</button>
+        {/* <button className="btn-primary py-3 mt-4">저장 하기</button> */}
       </div>
     </div>
   );
