9587b1463fe16aa2dbc84e6d5b023a7ce087cb45
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-04 10:22:55 +0900

    [Add] useQuery in ShareCard.jsx

--- src/components/schedule/ShareCard.jsx
+++ src/components/schedule/ShareCard.jsx
@@ -1,33 +1,43 @@
-import React, { useEffect, useState } from "react";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import { useLocation, useNavigate } from "react-router-dom";
-import { scheduleAPIs } from "../../api/api";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { api } from "../../api/api";
 import ShareCardBookmark from "./ShareCardBookmark";
+import { useQuery } from "react-query";
+import Spinner from "../Spinner/Spinner";
 
 function ShareCard() {
-  const [shareData, setShareData] = useState();
-  const location = useLocation();
-
-  useEffect(() => {
-    scheduleAPIs.getShareSchedule().then(res => setShareData(res.data.data));
-  }, [location.key]);
-
-  return (
-    <div className="grid place-items-center h-screen">
-      <span className="m-5 font-normal text-lg">다른 뚜벅이들의 제주 일정을 참고해보세요!</span>
-      {shareData?.map(item => {
-        return <ShareCardComponent key={item.id} item={item} />;
-      })}
-    </div>
-  );
+  const { error, isLoading, data, refetch } = useQuery("shareCards", () => {
+    return api.get("/trip");
+  });
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (error) {
+    return <div>{error}</div>;
+  }
+  if (data) {
+    const schedules = data.data.data;
+    return (
+      <div className="grid place-items-center h-screen">
+        <span className="m-5 font-normal text-lg">다른 뚜벅이들의 제주 일정을 참고해보세요!</span>
+        {schedules?.map(item => {
+          return <ShareCardComponent key={item.id} item={item} />;
+        })}
+      </div>
+    );
+  }
 }
 
 function ShareCardComponent({ item }) {
-  const { id, title, startAt, endAt } = item;
+  const { id, title, startAt, endAt, bookmarked } = item;
   const navigate = useNavigate();
+  const bookmarkHandler = () => {};
 
   return (
-    <div className="group w-96 h-28 bg-white1 rounded-md shadow-lg mt-5 flex flex-row hover:bg-green1 relative">
+    <Link
+      to={`/schedule/share/detail/${id}`}
+      className="group w-96 h-28 bg-white1 rounded-md shadow-lg mt-5 flex flex-row hover:bg-green1 relative cursor-pointer"
+    >
       <div className="flex space-x-20">
         <div className="flex flex-col m-6 p-3 w-48">
           <span className="font-bold group-hover:text-white1">{title}</span>
@@ -35,17 +45,11 @@ function ShareCardComponent({ item }) {
             {startAt}-{endAt}
           </span>
         </div>
-        <div className="flex flex-row m-5 ">
-          <NavigateNextIcon
-            className="mt-5 cursor-pointer group-hover:fill-white1"
-            onClick={() => navigate(`/schedule/share/detail/${id}`)}
-          />
-        </div>
       </div>
       <div>
-        <ShareCardBookmark />
+        <ShareCardBookmark bookmarked={bookmarked} bookmarkHandler={bookmarkHandler} />
       </div>
-    </div>
+    </Link>
   );
 }
 
--- src/components/schedule/ShareCardBookmark.jsx
+++ src/components/schedule/ShareCardBookmark.jsx
@@ -1,13 +1,24 @@
 import StarOutlineRoundedIcon from "@mui/icons-material/StarOutlineRounded";
+import StarRoundedIcon from "@mui/icons-material/StarRounded";
 
-const ShareCardBookmark = () => {
+const ShareCardBookmark = ({ bookmarked, bookmarkHandler }) => {
   return (
     <div>
-      <StarOutlineRoundedIcon
-        className="absolute top-1 right-1 cursor-pointer hover:scale-125"
-        fontSize="large"
-        sx={{ color: "#ffd740" }}
-      />
+      {bookmarked ? (
+        <StarRoundedIcon
+          className="absolute top-4 right-4 cursor-pointer hover:scale-125"
+          fontSize="large"
+          sx={{ color: "#ffd740" }}
+          onClick={bookmarkHandler}
+        />
+      ) : (
+        <StarOutlineRoundedIcon
+          className="absolute top-4 right-4 cursor-pointer hover:scale-125"
+          fontSize="large"
+          sx={{ color: "#ffd740" }}
+          onClick={bookmarkHandler}
+        />
+      )}
     </div>
   );
 };
