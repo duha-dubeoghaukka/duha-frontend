a85bc3be58cc5690a14f6d3a48969c06278f94c3
marksenee <mark233891@gmail.com>
2022-10-10 16:00:48 +0900

    Feat: 일정 공유 링크 등록 및 css 수정

--- src/components/schedule/ScheduleCard.jsx
+++ src/components/schedule/ScheduleCard.jsx
@@ -7,6 +7,8 @@ import { useDispatch } from "react-redux";
 import { __getSchedules, __deleteSchedule } from "../../redux/modules/schedules";
 import { useSelector } from "react-redux";
 import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
+import ShareIcon from "@mui/icons-material/Share";
+import { color } from "@mui/system";
 
 function ScheduleCard() {
   const dispatch = useDispatch();
@@ -62,26 +64,36 @@ function ScheduleCardComponent({ title, startDate, endDate, id, isPublic, onDele
     navigate(`/schedule/update/${id}`, { state: [title, startDate, endDate, isPublic] });
   };
 
+  const onShareLink = () => {
+    console.log("test");
+  };
+
   return (
-    <div className="w-96 h-28 bg-white1 rounded-md shadow-lg mt-5 flex flex-row">
-      <div
-        className="pr-32"
-        onClick={() => {
-          navigate(`${id}/1`);
-          setItem();
-        }}
-      >
-        <div className="flex flex-col m-5 ">
-          <span className="mt-2	font-semibold">
-            {nights + "박" + allDays + "일" + " "}
-            {title}
-          </span>
-          <span className="mt-2 font-light text-sm">
-            {startDate}~{endDate}
-          </span>
+    <div className="w-96 h-32 bg-white1 rounded-md shadow-lg mt-5 flex flex-row">
+      <div className="flex flex-col mt-5 ml-5 mb-2">
+        <div
+          className="pr-32 h-24"
+          onClick={() => {
+            navigate(`${id}/1`);
+            setItem();
+          }}
+        >
+          <div className="flex flex-col">
+            <span className="mt-2	font-semibold">
+              {nights + "박" + allDays + "일" + " "}
+              {title}
+            </span>
+            <span className="mt-2 font-light text-sm">
+              {startDate}~{endDate}
+            </span>
+          </div>
+        </div>
+        <div className=" cursor-pointer" onClick={() => onShareLink()}>
+          <ShareIcon className="mr-1" sx={{ fontSize: 15, color: "#7FB77E" }} />
+          <span className="text-xs text-green1">일정 공유</span>
         </div>
       </div>
-      <div className="flex flex-row mt-5 ">
+      <div className="flex flex-row mt-5">
         <ModeEditOutlineOutlinedIcon className="mt-5 cursor-pointer" onClick={() => onUpdate(id)} />
         <DeleteOutlineIcon className="mt-5 ml-1 cursor-pointer" onClick={() => onDeleteSchedule(id)} />
       </div>
