38d367e378046ccf4853aeb7de5f72688aa6bca2
marksenee <mark233891@gmail.com>
2022-10-10 22:11:04 +0900

    Feat: 카카오 링크 연결 utils 파일 수정

---
+++ src/utils/shareKakaoLink.js
@@ -0,0 +1,32 @@
+export const shareKakao = (route, title) => {
+  console.log("route", route);
+  if (window.Kakao) {
+    const kakao = window.Kakao;
+    if (!kakao.isInitialized()) {
+      kakao.init(process.env.REACT_APP_SHARE_KAKAO_LINK);
+    }
+
+    kakao.Link.sendDefault({
+      objectType: "feed",
+      content: {
+        title: title,
+        description: `${title}코스 확인하러가기`,
+        imageUrl: "https://ifh.cc/g/soo5OO.png",
+        link: {
+          mobileWebUrl: route,
+          webUrl: route
+          // androidExecParams: "test"
+        }
+      },
+      buttons: [
+        {
+          title: "코스 확인하러 가기",
+          link: {
+            mobileWebUrl: route,
+            webUrl: route
+          }
+        }
+      ]
+    });
+  }
+};
