4bb46ef4419acbab06e7fa39a134fd6f6307b7f6
marksenee <mark233891@gmail.com>
2022-10-10 22:10:43 +0900

    Feat: 일정 공유 링크 Url 연결 문제 -> addcourse에 현재 url 받아서 처리

--- src/pages/schedule/AddCourse.jsx
+++ src/pages/schedule/AddCourse.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/layout/Layout";
+import ShowModal from "../../components/modal/ShowModal";
 import DayItem from "../../components/schedule/DayItem";
+import { useLocation } from "react-router";
+import ShareLink from "../../components/shareLink/ShareLink";
 
 const AddCourse = () => {
+  const uri = window.location.href;
+
   return (
     <Layout title="일정 등록" highlight={"schedule/create"}>
+      <ShareLink uri={uri} />
       <DayItem />
     </Layout>
   );
