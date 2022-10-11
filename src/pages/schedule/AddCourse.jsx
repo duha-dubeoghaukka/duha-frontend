259b59f070a9247b387df2fcbd768d233729efd9
marksenee <mark233891@gmail.com>
2022-10-11 13:32:54 +0900

    refactoring

--- src/pages/schedule/AddCourse.jsx
+++ src/pages/schedule/AddCourse.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Layout from "../../components/layout/Layout";
 import DayItem from "../../components/schedule/DayItem";
 
