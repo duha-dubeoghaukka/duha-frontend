ff2dac24a0f761d6af7747341797362beeb718a5
marksenee <mark233891@gmail.com>
2022-10-11 13:32:28 +0900

    Feat: 일정 공유 링크 연결 버튼 삭제

--- src/pages/schedule/AddCourse.jsx
+++ src/pages/schedule/AddCourse.jsx
@@ -1,16 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../../components/layout/Layout";
-import ShowModal from "../../components/modal/ShowModal";
 import DayItem from "../../components/schedule/DayItem";
-import { useLocation } from "react-router";
-import ShareLink from "../../components/shareLink/ShareLink";
 
 const AddCourse = () => {
-  const uri = window.location.href;
-
   return (
     <Layout title="일정 등록" highlight={"schedule/create"}>
-      <ShareLink uri={uri} />
       <DayItem />
     </Layout>
   );
