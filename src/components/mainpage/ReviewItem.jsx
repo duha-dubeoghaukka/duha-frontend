d43e95ddcba5a78032ba28d62e242ffb32a61ce8
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-14 08:18:43 +0900

    [Add] 댓글 삭제 아이콘 추가.

--- src/components/mainpage/ReviewItem.jsx
+++ src/components/mainpage/ReviewItem.jsx
@@ -1,19 +1,27 @@
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const ReviewItem = ({ data }) => {
   const { review, reviewer } = data;
   return (
-    <div>
-      <div className="flex items-center mb-[24px]">
-        <div className="mr-[16px]">
-          <AccountCircleIcon fontSize="large" />
+    <div className="flex justify-between">
+      <div>
+        <div className="flex items-center mb-[24px]">
+          <div className="mr-[16px]">
+            <AccountCircleIcon fontSize="large" />
+          </div>
+          <div>
+            <p className="text-[16px]">{reviewer}</p>
+          </div>
         </div>
         <div>
-          <p className="text-[16px]">{reviewer}</p>
+          <p className="text-[16px]">{review}</p>
         </div>
       </div>
       <div>
-        <p className="text-[16px]">{review}</p>
+        <div className="cursor-pointer">
+          <DeleteIcon fontSize="large" />
+        </div>
       </div>
     </div>
   );
