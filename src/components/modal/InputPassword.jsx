0eeb06ecde621e22388d85f78c7d93ca38a31a0e
marksenee <mark233891@gmail.com>
2022-10-13 15:00:25 +0900

    Feat: 회원탈퇴 api 연동 및 로그아웃 처리

--- src/components/modal/InputPassword.jsx
+++ src/components/modal/InputPassword.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { userInfoAPIs } from "../../api/api";
+import useInput from "../../hooks/useInput";
+import { logout } from "../../utils/logout";
 
 function InputPassword() {
+  const [password, setPassword, onChangePassword] = useInput();
+  const navigate = useNavigate();
+
+  const onDeleteUser = () => {
+    let data = {
+      password
+    };
+    userInfoAPIs
+      .deleteUser(data)
+      .then(res => {
+        if (res.data.isSuccess) {
+          alert("회원 탈퇴가 완료되었습니다.");
+          logout();
+          navigate(`/`);
+        } else {
+          alert(res.data.message);
+        }
+      })
+      .catch(err => console.log(err.response));
+  };
   return (
     <>
       <h4 className="text-lg font-semibold text-black2 text-lg">회원 확인</h4>
       <div className="m-7">
-        <input className="w-full input mt-2" placeholder="이메일을 입력하세요" />
-        <input className="w-full input mt-2" placeholder="비밀번호를 입력하세요" />
-        <button className="btn-primary w-full mt-5">회원 탈퇴</button>
+        <input
+          className="w-full input mt-2"
+          placeholder="비밀번호를 입력하세요"
+          type="password"
+          name="password"
+          value={password}
+          onChange={onChangePassword}
+        />
+        <button className="btn-primary w-full mt-5" onClick={() => onDeleteUser()}>
+          회원 탈퇴
+        </button>
       </div>
     </>
   );
