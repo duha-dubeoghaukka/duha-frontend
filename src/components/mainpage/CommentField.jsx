d38386156e770b77be02f2cece113cef62403a43
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-14 07:58:32 +0900

    [Add] 로그인되어 있지 않을 시 댓글 입력창과 등록 버튼 막아놓기.

--- src/components/mainpage/CommentField.jsx
+++ src/components/mainpage/CommentField.jsx
@@ -1,4 +1,6 @@
-const CommentField = ({ comment, setComment }) => {
+import checkIsLoggedIn from "../../utils/checkIsLoggedIn";
+
+const CommentField = ({ comment, setComment, commentRegisterHandler }) => {
   const commentChangeHandler = event => {
     setComment(event.target.value);
   };
@@ -6,11 +8,19 @@ const CommentField = ({ comment, setComment }) => {
     <div className="grid grid-cols-[1fr_80px] gap-3">
       <input
         type="text"
-        className="border-green1 border-2 rounded-lg p-2 pl-5 text-black1"
+        className="border-green1 border-2 rounded-lg p-2 pl-5 text-black1 disabled:border-gray-200 disabled:bg-gray-200 placeholder:text-white1 disabled:placeholder:text-black1 disabled:cursor-not-allowed"
         value={comment}
+        placeholder="로그인 후에 댓글 입력이 가능합니다."
         onChange={commentChangeHandler}
+        disabled={!checkIsLoggedIn()}
       />
-      <button className="bg-green1 rounded-lg text-white1 font-bold p-2 cursor-pointer hover:brightness-90">작성</button>
+      <button
+        className="bg-green1 rounded-lg text-white1 font-bold p-2 cursor-pointer hover:brightness-90 disabled:bg-gray-400 disabled:cursor-not-allowed disabled:hover:brightness-100"
+        onClick={commentRegisterHandler}
+        disabled={!checkIsLoggedIn()}
+      >
+        작성
+      </button>
     </div>
   );
 };
--- src/components/mainpage/Comments.jsx
+++ src/components/mainpage/Comments.jsx
@@ -4,9 +4,12 @@ import { useState } from "react";
 
 const Comments = ({ category }) => {
   const [comment, setComment] = useState("");
+  const commentRegisterHandler = () => {
+    console.dir(comment);
+  };
   return (
     <div>
-      <CommentField comment={comment} setComment={setComment} />
+      <CommentField comment={comment} setComment={setComment} commentRegisterHandler={commentRegisterHandler} />
       <CommentList />
     </div>
   );
