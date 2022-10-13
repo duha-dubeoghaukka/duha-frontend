895b78be4ce1c1384d473af56d35996d3b0dfa8e
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-14 07:48:35 +0900

    [Add] 댓글 작성창 추가.

--- src/components/mainpage/CommentField.jsx
+++ src/components/mainpage/CommentField.jsx
@@ -1,5 +1,18 @@
-const CommentField = () => {
-  return <div></div>;
+const CommentField = ({ comment, setComment }) => {
+  const commentChangeHandler = event => {
+    setComment(event.target.value);
+  };
+  return (
+    <div className="grid grid-cols-[1fr_80px] gap-3">
+      <input
+        type="text"
+        className="border-green1 border-2 rounded-lg p-2 pl-5 text-black1"
+        value={comment}
+        onChange={commentChangeHandler}
+      />
+      <button className="bg-green1 rounded-lg text-white1 font-bold p-2 cursor-pointer hover:brightness-90">작성</button>
+    </div>
+  );
 };
 
 export default CommentField;
--- src/components/mainpage/Comments.jsx
+++ src/components/mainpage/Comments.jsx
@@ -1,9 +1,13 @@
 import CommentField from "./CommentField";
+import CommentList from "./CommentList";
+import { useState } from "react";
 
 const Comments = ({ category }) => {
+  const [comment, setComment] = useState("");
   return (
     <div>
-      <CommentField />
+      <CommentField comment={comment} setComment={setComment} />
+      <CommentList />
     </div>
   );
 };
