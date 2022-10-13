055b47769c95d48cc70d34dfa5eeacecccbad968
marksenee <mark233891@gmail.com>
2022-10-13 18:13:31 +0900

    Fix: 소셜 로그인 url 수정

--- src/utils/socialLoginUtils/google.js
+++ src/utils/socialLoginUtils/google.js
@@ -1,3 +1,3 @@
 const googleApiKey = process.env.REACT_APP_API_GOOGLE_KEY;
-const redirectUri = "http://dduha.s3-website.ap-northeast-2.amazonaws.com/auth/google/callback";
+const redirectUri = "https://dduha.site/auth/google/callback";
 export const GOOGLE_AUTH_URI = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${googleApiKey}&redirect_uri=${redirectUri}&response_type=code&scope=email`;
--- src/utils/socialLoginUtils/kakao.js
+++ src/utils/socialLoginUtils/kakao.js
@@ -1,3 +1,3 @@
 const kakaoApiKey = process.env.REACT_APP_API_KAKAO_KEY;
-const redirectUri = "http://dduha.s3-website.ap-northeast-2.amazonaws.com/auth/kakao/callback";
+const redirectUri = "https://dduha.site/auth/kakao/callback";
 export const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoApiKey}&redirect_uri=${redirectUri}&response_type=code`;
