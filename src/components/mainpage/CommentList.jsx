44da9c606733203e7eda519e58e4f27a3f4dfc8c
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-14 08:53:45 +0900

    [Add] 댓글 없을 때의 메시지 추가.

--- src/components/mainpage/CommentList.jsx
+++ src/components/mainpage/CommentList.jsx
@@ -1,16 +1,23 @@
 import ReviewItem from "./ReviewItem";
 
 const CommentList = ({ comments, commentDeleteHandler }) => {
+  const isCommentsEmpty = comments.length === 0;
   return (
     <div>
       <div className="mt-3">
         <div className="bg-white1 rounded-md px-5 md:px-10 py-3 md:py-5">
           <p className="text-base md:text-lg font-semibold mb-3">댓글</p>
-          <div className="grid gap-[44px]">
-            {comments.map(comment => {
-              return <ReviewItem key={comment.id} data={comment} commentDeleteHandler={commentDeleteHandler} />;
-            })}
-          </div>
+          {isCommentsEmpty ? (
+            <div>
+              <p className="text-center text-black1">아직 댓글이 존재하지 않습니다.</p>
+            </div>
+          ) : (
+            <div className="grid gap-[44px]">
+              {comments.map(comment => {
+                return <ReviewItem key={comment.id} data={comment} commentDeleteHandler={commentDeleteHandler} />;
+              })}
+            </div>
+          )}
         </div>
       </div>
     </div>
