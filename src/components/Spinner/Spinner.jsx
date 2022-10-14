8d6a13c32beb102843da3fa5feae7ce278e0f548
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-15 07:56:02 +0900

    [Add] React Loader Spinner 추가.

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,7 +1,17 @@
+import { ColorRing } from "react-loader-spinner";
+
 const Spinner = () => {
   return (
     <div className="w-full h-[100vh] flex justify-center items-center">
-      <img src="/assets/spinner.gif" alt="Loading Spinner" className="w-20" />
+      <ColorRing
+        visible={true}
+        height="120"
+        width="120"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+      />
     </div>
   );
 };
