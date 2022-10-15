24c76f2243bb467197ca511204ddef717aceff75
marksenee <mark233891@gmail.com>
2022-10-15 20:48:52 +0900

    Fix: 일정, 마이페이지 로그인 여부 확인

--- src/pages/menu/ScheduleInfo.jsx
+++ src/pages/menu/ScheduleInfo.jsx
@@ -1,9 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import Layout from "../../components/layout/Layout";
+import { routingLoginPage } from "../../utils/routingLoginPage";
 
 const ScheduleInfo = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    routingLoginPage(navigate);
+  }, []);
+
   return (
     <Layout title="일정" highlight={"schedule"}>
       <Link to="/schedule" className="card">
