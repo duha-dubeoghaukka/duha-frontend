0f0ccc636808cdebda4462c529d1f157c1f0c068
marksenee <mark233891@gmail.com>
2022-10-18 00:01:26 +0900

    Fix: nickName 로직 변경

--- src/utils/decodeToken.js
+++ src/utils/decodeToken.js
@@ -5,8 +5,9 @@ const decodeToken = token => {
     const decode_token = jwt_decode(token);
     const array = decode_token.sub.split(":");
     const nickName = array[1];
-    const platform = array[2] === "null" ? "" : array[2] + "_";
-    return platform + nickName;
+    // const platform = array[2] === "null" ? "" : array[2] + "_";
+    // return platform + nickName;
+    return nickName;
   } else {
     return null;
   }
