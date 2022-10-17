dc771b99f4b77e6df62e4a98187adcb9f3ebaeb5
marksenee <mark233891@gmail.com>
2022-10-17 22:27:34 +0900

    Fix: 소셜로그인 -> 회원 탈퇴시 버튼 없앰

--- src/components/form/EditInfoSocialLogin.jsx
+++ src/components/form/EditInfoSocialLogin.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ShowEmailModal from "../modal/ShowEmailModal";
 
-function EditInfoSocialLogin({ nickName, isNickName, onSubmit, onChangeNickName, modalHandler, isModal }) {
+function EditInfoSocialLogin({ nickName, isNickName, onSubmit, onChangeNickName }) {
   return (
     <div className="relative w-full md:w-[500px] mx-auto">
       <input className="input mt-2 font-bold" placeholder="닉네임" name="nickname" value={nickName || ""} onChange={onChangeNickName} />
@@ -9,10 +9,6 @@ function EditInfoSocialLogin({ nickName, isNickName, onSubmit, onChangeNickName,
         <button className="btn-primary mt-3 mb-3 disabled:bg-[#B1D7B4]" onClick={() => onSubmit()} disabled={!isNickName}>
           <p>변경 완료</p>
         </button>
-        <button className="btn-white" onClick={() => modalHandler()}>
-          <p>회원탈퇴</p>
-        </button>
-        <ShowEmailModal show={isModal} modalHandler={modalHandler} category={"회원탈퇴"} />
       </div>
     </div>
   );
