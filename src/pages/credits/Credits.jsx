6eb62268a51c75436d0fe0f7df3a6b8f977e80de
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-18 07:48:37 +0900

    [Add] Credits.jsx

---
+++ src/pages/credits/Credits.jsx
@@ -0,0 +1,11 @@
+import Layout from "../../components/layout/Layout";
+
+const Credits = () => {
+  return (
+    <Layout title={"출처 및 저작권자 정보"} highlight={""} isLoggedIn={false} isFooterFixed={true}>
+      wow
+    </Layout>
+  );
+};
+
+export default Credits;
