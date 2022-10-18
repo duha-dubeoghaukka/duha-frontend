b5bf32b176ff24d128f4b8340b0ac8ffc569f14a
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-19 07:16:38 +0900

    [Add] goFirstPage() & goLastPage().

--- src/components/mainpage/Pagination.jsx
+++ src/components/mainpage/Pagination.jsx
@@ -1,27 +1,52 @@
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
 
 const Pagination = ({ currentPage, numberOfPages, setCurrentPage }) => {
+  const goFirstPage = () => {
+    setCurrentPage(0);
+  };
+  const goLastPage = () => {
+    setCurrentPage(numberOfPages - 1);
+  };
   const goPreviousPage = () => {
     setCurrentPage(currentPage - 1);
   };
   const goNextPage = () => {
     setCurrentPage(currentPage + 1);
   };
+  const isFirstPage = currentPage === 0;
+  const isLastPage = currentPage === numberOfPages - 1;
   return (
     <div className="flex justify-center">
-      <div onClick={goPreviousPage}>
-        <ChevronLeftIcon fontSize="large" />
-        <p>이전</p>
-      </div>
+      {!isFirstPage && (
+        <div className="cursor-pointer" onClick={goFirstPage}>
+          <KeyboardDoubleArrowLeftIcon fontSize="large" />
+        </div>
+      )}
+      {!isFirstPage && (
+        <div onClick={goPreviousPage} className="flex cursor-pointer">
+          <ChevronLeftIcon fontSize="large" />
+          <p>이전</p>
+        </div>
+      )}
       <div>
         <p>
-          {currentPage} / {numberOfPages}
+          {currentPage + 1} / {numberOfPages}
         </p>
       </div>
-      <div onClick={goNextPage}>
-        <ChevronRightIcon fontSize="large" />
-      </div>
+      {!isLastPage && (
+        <div onClick={goNextPage} className="flex cursor-pointer">
+          <p>다음</p>
+          <ChevronRightIcon fontSize="large" />
+        </div>
+      )}
+      {!isLastPage && (
+        <div className="cursor-pointer" onClick={goLastPage}>
+          <KeyboardDoubleArrowRightIcon fontSize="large" />
+        </div>
+      )}
     </div>
   );
 };
