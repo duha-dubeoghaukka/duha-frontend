b1c8642186b6f1900170208a17b2c5590265ca32
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-19 07:09:03 +0900

    [Add] Pagination.jsx 수정.

--- src/components/mainpage/Pagination.jsx
+++ src/components/mainpage/Pagination.jsx
@@ -1,12 +1,20 @@
-import Page from "./Page";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 const Pagination = ({ currentPage, numberOfPages, setCurrentPage }) => {
   const pages = [...Array.from({ length: numberOfPages }).keys()];
   return (
     <div className="flex justify-center">
-      {pages.map(page => {
-        return <Page key={page} pageNumber={page} isActive={currentPage === page} setCurrentPage={setCurrentPage} />;
-      })}
+      <div>
+        <ChevronLeftIcon fontSize="large" />
+        <p>이전</p>
+      </div>
+      <div>
+        <p>{currentPage}</p>
+      </div>
+      <div>
+        <ChevronRightIcon fontSize="large" />
+      </div>
     </div>
   );
 };
