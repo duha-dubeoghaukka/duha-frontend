169ff2e4f0c697d4394587d2589b75537faab306
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-19 07:11:14 +0900

    [Add] goPreviousPage() & goNextPage().

--- src/components/mainpage/Pagination.jsx
+++ src/components/mainpage/Pagination.jsx
@@ -2,17 +2,24 @@ import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 const Pagination = ({ currentPage, numberOfPages, setCurrentPage }) => {
-  const pages = [...Array.from({ length: numberOfPages }).keys()];
+  const goPreviousPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+  const goNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
   return (
     <div className="flex justify-center">
-      <div>
+      <div onClick={goPreviousPage}>
         <ChevronLeftIcon fontSize="large" />
         <p>이전</p>
       </div>
       <div>
-        <p>{currentPage}</p>
+        <p>
+          {currentPage} / {numberOfPages}
+        </p>
       </div>
-      <div>
+      <div onClick={goNextPage}>
         <ChevronRightIcon fontSize="large" />
       </div>
     </div>
