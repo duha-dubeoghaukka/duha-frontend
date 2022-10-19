3e4cc3e7319d68b0946ae291e6db08f2a4fca119
marksenee <mark233891@gmail.com>
2022-10-19 21:50:33 +0900

    Fix: 마이페이지 레이아웃 없는 스피너 적용

--- src/components/mypage/FavoriteItemList.jsx
+++ src/components/mypage/FavoriteItemList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { mypageAPIs } from "../../api/api";
 import Spinner from "../Spinner/Spinner";
 import Card from "../card/Card";
+import NonLayoutSpinner from "../Spinner/NonLayoutSpinner";
 
 function FavoriteItemList() {
   const [favoriteScheduleNum, setFavoriteScheduleNum] = useState();
@@ -55,7 +56,7 @@ function FavoriteItemList() {
           return <Card key={index} title={item.title} route={item.route} />;
         })
       ) : (
-        <Spinner />
+        <NonLayoutSpinner />
       )}
     </div>
   );
