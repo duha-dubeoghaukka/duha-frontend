216aff43090fafbca07a3e8af54ea8c89b860e2a
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-19 13:11:22 +0900

    [Fix] 이동수단과 짐배달 링크가 새 창에서 열리도록 수정.

--- src/pages/information/transportation/TransportationItem.jsx
+++ src/pages/information/transportation/TransportationItem.jsx
@@ -1,43 +1,10 @@
 const TransportationItem = ({ transportation }) => {
   const { title, address, description, imageURL } = transportation;
-  // --- Front-End의 CORS 에러의 이유로 Hard-Coding ---
-  // const { isLoading, error, data } = useQuery(name, () => {
-  //   return axios.get(`https://cors-anywhere.herokuapp.com/${address}`);
-  // });
-  // if (isLoading) {
-  //   return <Spinner />;
-  // }
-  // if (error) {
-  //   return <div>{error}</div>;
-  // }
-  // if (data) {
-  // const stringHTML = data.data;
-  // const html = parser(stringHTML);
-  // const tags = html.props.children[0].props.children;
-  // const metaTags = tags.filter(tag => {
-  //   return tag.props?.property?.includes("og") || tag.props?.property === "description";
-  // });
-  // let metaData;
-  // if (metaTags.length === 0) {
-  // } else {
-  //   metaData = metaTags.map(tag => {
-  //     const property = tag.props.property;
-  //     let title;
-  //     if (property.includes(":")) {
-  //       title = tag.props.property.split(":")[1];
-  //     } else {
-  //       title = tag.props.property;
-  //     }
-  //     const content = tag.props.content;
-  //     return { title, content };
-  //   });
-  // }
   return (
-    <div
+    <a
       className="border-2 border-green1 rounded-lg mb-3 p-3 grid md:grid-cols-[1fr_160px] cursor-pointer hover:brightness-75"
-      onClick={() => {
-        window.location.href = address;
-      }}
+      href={address}
+      target="_blank"
     >
       <div className="grid gap-1">
         <p className="text-black1 font-bold text-lg">{title}</p>
@@ -47,7 +14,7 @@ const TransportationItem = ({ transportation }) => {
       <div className="ml-3 hidden md:block">
         <img src={imageURL} alt={title} className="rounded-lg h-full object-cover" />
       </div>
-    </div>
+    </a>
   );
 };
 
