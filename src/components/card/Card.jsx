466ec2a1972dff0996715a7866c2cfdeb9d7806d
marksenee <mark233891@gmail.com>
2022-10-19 20:24:04 +0900

    Design: 메인페이지 css 수정

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { Link } from "react-router-dom";
+import { Grid, Box } from "@material-ui/core";
 
-function Card({ title, route }) {
+function Card({ item }) {
+  const { title, route, description, background } = item;
+
   return (
-    <Link to={route} className="card-layout">
-      <div className="font-bold text-black2 text-sm md:text-lg">{title}</div>
-      <NavigateNextIcon />
-    </Link>
+    <Grid item xs={5} sm={5}>
+      <Link to={route} className="relative">
+        <Box className={`${background}`} p={"60px"}></Box>
+        <div className="absolute w-full h-full top-0 left-0 w-full h-full bg-[#757575] opacity-70 rounded-lg"></div>
+        <div className="absolute w-full h-full top-0 left-0 flex flex-col justify-start mt-8 ml-4">
+          <p className="text-[15px] w-full font-bold text-white1 md:text-[18px]">{title}</p>
+          <p className="mt-1 w-full text-[10px] text-white1 md:text-[12px]">
+            {description}
+            <br></br>확인해보세요
+          </p>
+        </div>
+      </Link>
+    </Grid>
   );
 }
 
