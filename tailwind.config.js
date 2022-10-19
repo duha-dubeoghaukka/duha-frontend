9366aae84d6c56f68e9b6c11b684bb0c80516cd1
marksenee <mark233891@gmail.com>
2022-10-19 20:42:35 +0900

    Add: package, tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ module.exports = {
         white1: "#F6F6F6",
         green1: "#7FB77E",
         grey1: "#757575"
+      },
+      backgroundImage: {
+        "main-spot": "url('https://i.ibb.co/6RYFXC0/image.jpg')",
+        "main-restuarant": "url('https://i.ibb.co/MhTXbMQ/image.jpg')",
+        "main-accomodation": "url('https://i.ibb.co/ZGR6ny5/image.jpg')",
+        "main-service": "url('https://i.ibb.co/x5h36pN/image.jpg')",
+        "main-schedule": "url('https://i.ibb.co/5x9qhRQ/image.jpg')",
+        "main-share-schedule": "url('https://i.ibb.co/N7dCWfc/image.jpg')",
+        "main-weather": "url('https://i.ibb.co/kG0N6r5/image.jpg')",
+        "main-sea": "url('https://i.ibb.co/02RvLxP/image.jpg')"
       }
     }
   },
