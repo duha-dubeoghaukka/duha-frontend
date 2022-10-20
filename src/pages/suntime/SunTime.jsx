875a5217b9bca429d4c723995aad9aa5020e842b
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-21 07:57:46 +0900

    [Add] SunTime.jsx

---
+++ src/pages/suntime/SunTime.jsx
@@ -0,0 +1,11 @@
+import Layout from "../../components/layout/Layout";
+
+const SunTime = () => {
+  return (
+    <Layout title="일출/일몰 시간 보기" highlight="mainpage/suntime">
+      wow
+    </Layout>
+  );
+};
+
+export default SunTime;
