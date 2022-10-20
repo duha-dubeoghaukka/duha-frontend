73ddf20464281c49a61c8ba14266d7f2bbbdea60
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-20 13:25:05 +0900

    [Fix] 댓글 디자인 수정.

--- src/pages/mypage/usercomments/Comment.jsx
+++ src/pages/mypage/usercomments/Comment.jsx
@@ -6,10 +6,9 @@ const Comment = ({ comment }) => {
         <div className="bg-white shadow-md p-2 rounded-md inline-block">
           <p className="text-sm text-black1 md:text-base">{review}</p>
         </div>
-        <div>
-          <p className="text-black1">
-            {reviewName} - {reviewedAt}
-          </p>
+        <div className="flex flex-col items-end">
+          <p className="text-black1">{reviewName}</p>
+          <p className="text-black1 text-xs">{reviewedAt}</p>
         </div>
       </div>
     </div>
