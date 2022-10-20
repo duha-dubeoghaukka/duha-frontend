52c6298454b7cdf9986b045c1765ff89e3220187
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-20 12:59:14 +0900

    [Add] API 요청 기능 추가.

--- src/pages/mypage/usercomments/Category.jsx
+++ src/pages/mypage/usercomments/Category.jsx
@@ -4,13 +4,13 @@ const Category = ({ title, isActive, setCurrentCategory }) => {
   };
   if (isActive) {
     return (
-      <div className="font-bold text-green1">
+      <div className="font-bold text-green1 text-lg">
         <p>{title}</p>
       </div>
     );
   } else {
     return (
-      <div className="font-bold text-black1 cursor-pointer hover:brightness-[3]" onClick={categoryClickHandler}>
+      <div className="font-bold text-black1 cursor-pointer hover:brightness-[3] text-lg" onClick={categoryClickHandler}>
         <p>{title}</p>
       </div>
     );
--- src/pages/mypage/usercomments/UserComments.jsx
+++ src/pages/mypage/usercomments/UserComments.jsx
@@ -3,8 +3,26 @@ import checkIsLoggedIn from "../../../utils/checkIsLoggedIn";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Categories from "./Categories";
+import { useQuery } from "react-query";
+import { api } from "../../../api/api";
+import Spinner from "../../../components/Spinner/Spinner";
 
 const UserComments = () => {
+  const { isLoading, error, data } = useQuery("userComments", () => {
+    switch (currentCategory) {
+      case "관광":
+        return api.get(`/auth/mypage/touristspotreview`);
+        break;
+      case "맛집":
+        return api.get(`/auth/mypage/restaurantreview`);
+        break;
+      case "숙소":
+        return api.get(`/auth/mypage/accommodationreview`);
+        break;
+      default:
+        break;
+    }
+  });
   const navigator = useNavigate();
   const [currentCategory, setCurrentCategory] = useState("관광");
   useEffect(() => {
@@ -13,11 +31,21 @@ const UserComments = () => {
       navigator("/login");
     }
   }, []);
-  return (
-    <Layout title="내가 작성한 댓글 목록" highlight="mypage/comments">
-      <Categories currentCategory={currentCategory} setCurrentCategory={setCurrentCategory} />
-    </Layout>
-  );
+  if (isLoading) {
+    return <Spinner title={"내가 작성한 댓글 목록"} />;
+  }
+  if (error) {
+    return <div>{error}</div>;
+  }
+  if (data) {
+    const comments = data.data.data;
+    console.dir(comments);
+    return (
+      <Layout title="내가 작성한 댓글 목록" highlight="mypage/comments">
+        <Categories currentCategory={currentCategory} setCurrentCategory={setCurrentCategory} />
+      </Layout>
+    );
+  }
 };
 
 export default UserComments;
