5419c55f09d2c337b88fc779985fc1083057fd34
marksenee <mark233891@gmail.com>
2022-10-21 18:23:38 +0900

    Fix: 메인페이지 밝기 수정

--- src/components/card/MainCategoryCard.jsx
+++ src/components/card/MainCategoryCard.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { Link } from "react-router-dom";
 import { Grid, Box } from "@material-ui/core";
 
@@ -12,7 +11,7 @@ function MainCategoryCard({ item }) {
         <Box className={`${background}`} p={"60px"}>
           {none ? null : (
             <>
-              <div className="absolute w-full h-full top-0 left-0 w-full h-full bg-[#757575] opacity-70 rounded-lg"></div>
+              <div className="absolute w-full h-full top-0 left-0 w-full h-full bg-[#757575] opacity-70 rounded-lg brightness-75"></div>
               <div className="absolute w-full h-full top-0 left-0 flex flex-col justify-start mt-8 ml-4">
                 <p className="text-[15px] w-full font-bold text-white1 md:text-[18px]">{title}</p>
                 <p className="mt-1 w-full text-[10px] text-white1 md:text-[12px]">
