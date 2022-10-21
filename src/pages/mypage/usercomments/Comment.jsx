016fce297ea16a75df2e9d84756aa87539e96d36
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-21 12:37:32 +0900

    [Update] 유저 댓글 디자인 수정.

--- src/pages/mypage/usercomments/Comment.jsx
+++ src/pages/mypage/usercomments/Comment.jsx
@@ -18,16 +18,14 @@ const Comment = ({ comment, currentCategory }) => {
   const { reviewName, reviewedAt, review, itemId } = comment;
   return (
     <div className="my-3">
-      <div className="flex justify-between items-center">
-        <div className="bg-white shadow-md p-2 rounded-md inline-block">
-          <p className="text-sm text-black1">{review}</p>
-        </div>
-        <div className="flex flex-col items-end flex-shrink-0 ml-3">
-          <Link to={`/${category}/${itemId}`} className="text-black1" className="hover:underline hover:text-blue-500">
-            {reviewName}
-          </Link>
-          <p className="text-black1 text-xs">{reviewedAt}</p>
-        </div>
+      <div className="flex justify-between items-center mb-1">
+        <Link to={`/${category}/${itemId}`} className="text-black1" className="hover:underline hover:text-blue-500">
+          {reviewName}
+        </Link>
+        <p className="text-black1 text-xs">{reviewedAt}</p>
+      </div>
+      <div className="bg-white1 p-2 shadow-md rounded-lg inline-block">
+        <p className="text-black1">{review}</p>
       </div>
     </div>
   );
