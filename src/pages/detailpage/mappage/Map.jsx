6daeae38e7acfd59e337d20e666879f6df905dc8
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-24 14:33:55 +0900

    [Refactor] MapLoading.jsx.

--- src/pages/detailpage/mappage/Map.jsx
+++ src/pages/detailpage/mappage/Map.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import CloseIcon from "@mui/icons-material/Close";
+import MapLoading from "./MapLoading";
 
 const Map = ({ setIsMapModalOpen, latitude, longitude }) => {
   let validity = false;
@@ -20,11 +21,7 @@ const Map = ({ setIsMapModalOpen, latitude, longitude }) => {
   return (
     <div className="relative">
       <div id="map" className="w-[80vw] h-[80vw] md:w-[500px] md:h-[500px] rounded-lg">
-        {validity || (
-          <div className="w-full h-full bg-white1 rounded-lg p-5">
-            <p className="text-black1 font-bold text-[26px] text-center">지도 정보를 불러오는 중입니다.</p>
-          </div>
-        )}
+        {validity || <MapLoading />}
       </div>
       <div className="absolute top-1 right-1 cursor-pointer" onClick={closeClickHandler}>
         <CloseIcon fontSize="large" />
---
+++ src/pages/detailpage/mappage/MapLoading.jsx
@@ -0,0 +1,9 @@
+const MapLoading = () => {
+  return (
+    <div className="w-full h-full bg-white1 rounded-lg p-5">
+      <p className="text-black1 font-bold text-[26px] text-center">지도 정보를 불러오는 중입니다.</p>
+    </div>
+  );
+};
+
+export default MapLoading;
