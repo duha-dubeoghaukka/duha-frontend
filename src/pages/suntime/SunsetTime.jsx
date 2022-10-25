17184d844ed37fa22bfad6bd5d9b0c08241f3d91
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-10-25 10:43:07 +0900

    [Update] 일몰 이미지 변경.

--- src/pages/suntime/SunsetTime.jsx
+++ src/pages/suntime/SunsetTime.jsx
@@ -3,8 +3,8 @@ const SunsetTime = ({ sunsetHour, sunsetMinute }) => {
     <div className="relative">
       <img src="/assets/suntimeImages/Sunset.jpg" alt="Sunset" className="w-full h-[40vh] object-cover" />
       <div className="text-center absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
-        <p className="text-black1 font-bold text-3xl">일몰</p>
-        <p className="text-black1 font-bold text-5xl">
+        <p className="text-white1 font-bold text-3xl">일몰</p>
+        <p className="text-white1 font-bold text-5xl">
           {sunsetHour} : {sunsetMinute}
         </p>
       </div>
